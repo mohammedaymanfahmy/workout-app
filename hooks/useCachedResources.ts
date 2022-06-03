cc20672a6907639b65b8d64b727fb430c06f6899
Mohamed Ayman <muhamed.ayman.fahmy@gmail.com>
2022-06-03 14:57:42 +0200

    create cached resource Hook

--- hooks/useCachedResources.ts
+++ hooks/useCachedResources.ts
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import * as Font from "expo-font";
+import { clearWorkouts, initWorkouts } from "../storage/workouts";
+import { getWorouts } from "../storage/workouts";
 
 //custom Hook
 export const useCachedResources = () => {
@@ -9,6 +11,8 @@ export const useCachedResources = () => {
     //function implemintation
     async function LoadResourceAndDataAsync() {
       try {
+        //await clearWorkouts();
+        await initWorkouts();
         await Font.loadAsync({
           montserrat: require("../assets/fonts/Montserrat-Regular.ttf"),
           "montserrat-bold": require("../assets/fonts/Montserrat-Bold.ttf"),
